43471a62feb56df1130687b6b252c0e23a8387cb
KerryAlsace <kerrydougherty@gmail.com>
2016-11-13 14:15:29 -0500

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,4 +1,17 @@
 describe('ContactController', function () {
+  var $controller;
+
+  beforeEach(module('app'));
+
+  beforeEach(inject(function (_$controller_) {
+      $controller = _$controller_;
+  }));
 
 
+  it('should filter the results correctly', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      // $scope.vm holds all of our values
+  });
 });
